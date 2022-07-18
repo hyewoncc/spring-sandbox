74413567c16d727231a37278dbd8a4a101c79a8e
hyewoncc <hyewoncc@gmail.com>
2022-07-18 18:58:12 +0900

    새로운 테스트 추가

--- src/test/java/sandbox/SpringSandboxApplicationTests.java
+++ src/test/java/sandbox/SpringSandboxApplicationTests.java
@@ -1,5 +1,8 @@
 package sandbox;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +11,12 @@ class SpringSandboxApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@DisplayName("성공하는 테스트")
+	@Test
+	void test_successCase () {
+		assertThat(2).isEqualTo(2);
 	}
 
 }
